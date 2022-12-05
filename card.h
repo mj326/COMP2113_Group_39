1a83ded1509073678265caa10f3d229c8ad89138
siwookim1114 <63380176+siwookim1114@users.noreply.github.com>
2022-12-05 20:32:53 +0800

    Update card.h

--- card.h
+++ card.h
@@ -8,9 +8,10 @@
 #include <iterator>
 using namespace std;
 
-// Create namespace for the Deck
+// Create namespace for the card Deck
 namespace Card_namespace {
-
+	
+	//setting the values of each cards in Deck accordingtly to the game 
 	const int NumofCards = 52;
 	// Calculate Ace as 11
 	const int DIAMOND_A = 1; 
@@ -77,44 +78,44 @@ namespace Card_namespace {
 	const int CLUBS_Q = 430;
 }
 
-class Card // into Card_class 
+class Card // into Card_class 		
 {
 protected:
 	int value; // Actual value used
 	
 private:
-	int cards;
+	int cards;	
 	
 public:
-	int sut, number, cardName;
-	Card();
-	~Card();
-	Card(const Card & ca);
-	void setupCard(int cardName) ;
+	int sut, number, cardName;	//declaring variables of card suit, card number, and card name
+	Card();				//Card class initilization
+	~Card();			//Card class destructor
+	Card(const Card & ca);		//Card class initilization with input
+	void setupCard(int cardName) ;	//Card setup function with input
+		
+	void setupSuit(int cardName);	//Card suit setup function with input
+	void setupValue(int cardName);	//Card value setup function with input	
 	
-	void setupSuit(int cardName);
-	void setupValue(int cardName);
+	void setupNumber(int cardName);	//Card Number setup function with input
 	
-	void setupNumber(int cardName);
-	
-	char printSuit() ;
-	int printNum() ;
-	int printValue() ;
+	char printSuit() ;		//Card suit printing function
+	int printNum() ;		//Card number printing function
+	int printValue() ;		//Card value printing function
 };
 
-class Deck
+class Deck				//Deck class
 {
-	vector<Card> cards;
+	vector<Card> cards;		//Using vector as STL for deck 
 public:
-	Deck();
-	~Deck();
-	void mixDeck();
-	int printRemainCardsNum();
-	Card drawACard() ;
-	void init();
+	Deck();				//class Deck initilization
+	~Deck();			//class Deck destructor
+	void mixDeck();			//function to mix (shuffle) the deck
+	int printRemainCardsNum();	//function that shows the remaining cards left in deck
+	Card drawACard() ;		//function for drawing a card from the deck
+	void init();			//function to initilize the card deck
 };
 
-template <typename T>
+template <typename T>			//Using template for cards
 T printSum(vector <Card> &Hand) {
 	T num, sum = 0;
 	if (Hand.empty())
